9adf3d4e55258b18b9f56b34eb4a5a3aca141c9d
towalam <atowsiful@gmail.com>
2023-05-20 00:09:59 -0400

    idk

--- BattleBugs/UpsyDaisy.java
+++ BattleBugs/UpsyDaisy.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 import java.awt.Color;
+import java.sql.Array;
+
 import info.gridworld.actor.Actor;
 
 public class UpsyDaisy extends BattleBug2012
 {
+
     public UpsyDaisy(int str, int def, int spd, String name, Color col)
     {
             super(str, def, spd, name, col);
@@ -18,12 +21,25 @@ public class UpsyDaisy extends BattleBug2012
         Location goTo = new Location(5, 5); // Starting Location
         ArrayList<Location> puLocs = getPowerUpLocs(); // ALL powerup locations
         ArrayList<Actor> actors = getActors();  // Nearby actors/entites
+
+        ArrayList<BattleBug> enemies = new ArrayList<BattleBug>();
+        ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+
+        for (Actor current : actors) {
+            if (current instanceof BattleBug) {
+                enemies.add((BattleBug) current);
+            }
+            else if (current instanceof Projectile) {
+                projectiles.add((Projectile) current);
+            }
+        }
         
         
 
-        //CHECK TO SEE if there exists a PowerUp Location, if so then store the first location from the List into goTo
-        if (puLocs.size() != 0) {
+        if (puLocs.size() != 0) { // If it's not empty, then set the goTo to the nearest powerup
+            sortPowerUpsLocs(puLocs);
             goTo = puLocs.get(0);
+            
         }
 
 
@@ -34,29 +50,24 @@ public class UpsyDaisy extends BattleBug2012
         //Using the getDirection() method check to see if your bug is facing the desired direction dir
         //If so then call the move() method
         //if not then call turnTo() method towards the desired direction dir.
+
+        
+
+        
         
         if (dir == getDirection()) {
             move();
         }
-        else
+        else {
             turnTo(dir);
-
-    }
-
-    public void sortColorPowerUp(ArrayList<Location> powerUps, String color) {
-        int minPos;
-
-        for (int i = 0; i < powerUps.size() - 1; i++) {
-
-            minPos = i;
-            for (int j = i + 1; j < powerUps.size(); j++) {
-
-
-            }
-
         }
+
     }
 
+    
+
+    //      Getting the Locations of different powerups (Start)
+    //      Strength
     public ArrayList<Location> getStrengthLocs(ArrayList<PowerUp> allPowerUps) {
         
         ArrayList<Location> output = new ArrayList<Location>();
@@ -68,7 +79,8 @@ public class UpsyDaisy extends BattleBug2012
 
         return output;
     }
-
+ 
+    //      Defense
     public ArrayList<Location> getDefenseLocs(ArrayList<PowerUp> allPowerUps) {
         
         ArrayList<Location> output = new ArrayList<Location>();
@@ -81,6 +93,7 @@ public class UpsyDaisy extends BattleBug2012
         return output;
     }
 
+    //      Speed
     public ArrayList<Location> getSpeedLocs(ArrayList<PowerUp> allPowerUps) {
         
         ArrayList<Location> output = new ArrayList<Location>();
@@ -92,6 +105,49 @@ public class UpsyDaisy extends BattleBug2012
 
         return output;
     }
+    //      (End)
+
+    //      Distance between two locations 
+    public int distanceLocs(Location L1, Location L2) {
+        
+
+        return (int) Math.sqrt(Math.pow(L1.getRow() - L2.getRow(), 2) + Math.pow(L1.getCol() - L2.getCol(), 2));
+    }
+
+    //      Distance between the player and the desired Location
+    public int thisDistance(Location L1) {
+        return (int) Math.sqrt(Math.pow(L1.getRow() - this.getLocation().getRow(), 2) + Math.pow(L1.getCol() - this.getLocation().getCol(), 2));
+    }
+
+    //      Sorting PowerUps based on Locations (least to greatest distance) <-- Selection Sort
+    public void sortPowerUpsLocs(ArrayList<Location> powerUps) {
+
+        for (int i = 0; i < powerUps.size() - 1; i++) {
+
+            int index = i;
+
+            for (int j = i + 1; j < powerUps.size(); j++) {
+                if (thisDistance(powerUps.get(j)) < thisDistance(powerUps.get(index))) {
+                    index = j;
+                }
+            }
+
+            Location temp = powerUps.get(i);
+            powerUps.set(i, powerUps.get(index));
+            powerUps.set(index, temp);
+        }
+    }
+
+    //      Attacking Stretegically
+    public void dodgeAttack() {
+        
+    }
+
+    
+
+    
+
+    
 
     
 
